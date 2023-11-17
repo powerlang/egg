3c8e91c17bc1a272246d1e4c78561ad275228571
Javier <javier.pimas@Quorumsoftware.com>
2023-11-17 10:35:15 -0300

    egg-js: make sample server load compiler and compile a method

--- runtime/js/sample-server/index.js
+++ runtime/js/sample-server/index.js
@@ -2,12 +2,25 @@ import Bootstrapper from '../bootstrapper.js';
 import WebsideServer from '../webside/WebsideServer.js';
 
 var bootstrapper = new Bootstrapper();
-bootstrapper.loadKernelFile("image-segments/Kernel.json");
+bootstrapper.loadKernelFile("Kernel.json");
 const runtime = bootstrapper.runtime;
 
 const compiler = runtime.addSymbol_("Compiler");
 const kernel = bootstrapper.kernel.exports["Kernel"];
 const module = runtime.sendLocal_to_with_("load:", kernel, [compiler]);
+
+const namespace = runtime.sendLocal_to_("namespace", module);
+const classname = runtime.addSymbol_("SCompiler");
+
+const scompiler = runtime.sendLocal_to_with_("at:", namespace, [classname]);
+
+const object = bootstrapper.kernel.exports["Object"];
+const code = runtime.newString_("foo ^'foo'");
+const method = runtime.sendLocal_to_with_("compile:in:", scompiler, [code, object]);
+
+runtime.sendLocal_to_("install", method);
+const foo = runtime.sendLocal_to_("foo", object);
+console.log(foo.asLocalString());
 
 const webside = new WebsideServer("localhost", 9005, runtime);
 webside.start();
