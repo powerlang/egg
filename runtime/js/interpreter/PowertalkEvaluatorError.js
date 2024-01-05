3ae835204ece7441d5c3d3d16be982ec6122c16d
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2024-01-05 12:23:26 -0300

    interpreter: clone context when creating an error

--- runtime/js/interpreter/PowertalkEvaluatorError.js
+++ runtime/js/interpreter/PowertalkEvaluatorError.js
@@ -3,7 +3,7 @@
 let PowertalkEvaluatorError = class extends Error {
 	constructor(description, context) {
 		super(description);
-		this.context = context;
+		this.context = context.clone();
 	}
 
 	static signal_on_(description, context) {
