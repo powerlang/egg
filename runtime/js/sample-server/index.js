c5f09a85d15725d2861c572b293074b0ebd907eb
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2024-01-26 11:59:58 -0300

    specs: enable image segment builder submodule (to write JSON)

--- runtime/js/sample-server/index.js
+++ runtime/js/sample-server/index.js
@@ -5,7 +5,7 @@ var bootstrapper = new Bootstrapper();
 bootstrapper.loadKernelFile("Kernel.json");
 const runtime = bootstrapper.runtime;
 const kernel = bootstrapper.kernel.exports["Kernel"];
-const modules = ["Compiler", "STON"];
+const modules = ["Compiler", "ImageSegmentBuilder", "STON"];
 modules.forEach(name => {
     const symbol = runtime.addSymbol_(name);
     runtime.sendLocal_to_with_("load:", kernel, [symbol])});
