92a76160f4e831225b28b02e0e3a932efb926d8a
Javier Pimás <elpochodelagente@gmail.com>
2024-09-06 14:00:02 -0300

    cpp-vm: fixes for launcher, add a function to run bare tests

--- runtime/cpp/Launcher.cpp
+++ runtime/cpp/Launcher.cpp
@@ -5,8 +5,13 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 #include "Launcher.h"
+
+#include <algorithm>
+#include <Evaluator/Evaluator.h>
+
 #include "ImageSegment.h"
 #include "Util.h"
 #include "Bootstrapper.h"
@@ -40,15 +45,51 @@ Launcher::main(const int argc, const char** argv)
     for (int i = 0; i < argc; i++)
         args.push_back((Object*)runtime->newString_(argv[i]));
     
-     return 0;
+    start(runtime, kernel, args);
+    return 0;
 }
 
-void start(Runtime *runtime, HeapObject *kernel, std::vector<Object*> &args)
-{
+void start(Runtime *runtime, HeapObject *kernel, std::vector<Object*> &args) {
     HeapObject *name = runtime->sendLocal_to_("name", (Egg::Object*)kernel)->asHeapObject();
     std::cout << "The name of kernel module is " << name->asLocalString() << std::endl;
 
-    auto module = runtime->sendLocal_to_with_("loadModule:", (Object*)kernel, (Object*)args[1]);
+    std::cout << "Loading module " << args[1]->asHeapObject()->asLocalString() << std::endl;
+    auto module = runtime->sendLocal_to_with_("load:", (Object*)kernel, (Object*)args[1]);
 
-    runtime->sendLocal_to_with_("main:", module, args);
+    name = runtime->sendLocal_to_("name", module)->asHeapObject();
+    std::cout << "The name of loaded module is " << name->asLocalString() << std::endl;
+
+    //runtime->sendLocal_to_with_("main:", module, args);
+
+}
+
+void runBareTests(Runtime *runtime, HeapObject *kernel, std::vector<Object*> &args)
+{
+    auto segment = runtime->_bootstrapper->bareLoadModuleFromFile("Kernel.BareTests.ems");
+    segment->dumpObjects();
+    auto module = segment->_exports["__module__"];
+    auto methodDict = runtime->behaviorMethodDictionary_(runtime->behaviorOf_((Object*)module));
+    auto table = runtime->dictionaryTable_(methodDict);
+    std::vector<HeapObject*> methods;
+    for (int index = 2; index < table->size(); index += 2) {
+        auto symbol = table->slotAt_(index)->asHeapObject();
+        if (symbol != runtime->_nilObj && symbol->asLocalString().starts_with("test"))
+            methods.push_back(table->slotAt_(index + 1)->asHeapObject());
+    }
+
+    std::sort(methods.begin(), methods.end(),
+        [runtime](HeapObject *a, HeapObject *b) {
+            return std::strcmp((char*)runtime->methodSelector_(a), (char*)runtime->methodSelector_(b)) < 0;
+    });
+
+    for (auto method : methods) {
+       // auto result = runtime->_evaluator->invoke_with_(method, (Object*)runtime->_nilObj);
+        //runtime->_evaluator->evaluate();
+        auto selector = runtime->methodSelector_(method)->asLocalString();
+       // if (selector == "test161CreateDictionary")
+        {
+            auto result = runtime->sendLocal_to_(selector, (Object*)module);
+            ASSERT(result == (Object*)runtime->_trueObj);
+        }
+    }
 }
