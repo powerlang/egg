6cc91ca6b2173c8f90854a7f05b5fd8d4686a2a6
Javier Pimás <elpochodelagente@gmail.com>
2024-09-23 12:07:57 -0300

    cpp-vm: add platform-specific support for allocating mem in Windows

--- runtime/cpp/Memory.h
+++ runtime/cpp/Memory.h
@@ -6,7 +6,6 @@
 #ifndef _MEMORY_H_
 #define _MEMORY_H_
 
-#include <unistd.h>
 #include <cstdint>
 #include "Util.h"
 
@@ -17,16 +16,7 @@ uintptr_t ReserveMemory(uintptr_t base, uintptr_t size);
 void     CommitMemory(uintptr_t base, uintptr_t size);
 void     FreeMemory(uintptr_t base, uintptr_t size);
 
-static inline uintptr_t
-pagealign(uintptr_t addr)
-{
-    static int pagesize = -1;
-    if (pagesize == -1) {
-        pagesize = (int)sysconf(_SC_PAGESIZE);
-        ASSERT(pagesize != -1);
-    }
-    return align(addr, pagesize);
-}
+uintptr_t pagealign(uintptr_t addr);
 
 class HeapObject;
 
