cea3d331153df241789507b1560678643639d8af
Javier Pimás <elpochodelagente@gmail.com>
2025-04-24 14:19:20 -0300

    [vm-cpp] add support for arm64 in ffi

--- runtime/cpp/Posix/FFIGlue.cpp
+++ runtime/cpp/Posix/FFIGlue.cpp
@@ -24,15 +24,25 @@ uintptr_t Egg::LoaderHandle() {
 
 const char* Egg::PlatformName() {
 #if defined(__x86_64__)
-#if defined(__linux__)
-    return "x86_64-linux-gnu";
-#elif defined(__APPLE__)
-    return "x86_64-darwin";
-#elif defined(_WIN32)
-    return "x86_64-win32";
-#else
-    return "x86_64-unknown";
-#endif
+    #if defined(__linux__)
+        return "x86_64-linux-gnu";
+    #elif defined(__APPLE__)
+        return "x86_64-darwin";
+    #elif defined(_WIN32)
+        return "x86_64-win32";
+    #else
+        return "x86_64-unknown";
+    #endif
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    #if defined(__linux__)
+    return "aarch64-linux-gnu";
+    #elif defined(__APPLE__)
+    return "aarch64-darwin";
+    #elif defined(_WIN32)
+    return "aarch64-win32";
+    #else
+    return "aarch64-unknown";
+    #endif
 #elif defined(__riscv) && __riscv_xlen == 64
     return "riscv64-linux-gnu";
 #else
